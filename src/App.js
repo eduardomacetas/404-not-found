e37d3ac5a19468f29e8723aa3ddbae7df083ce2b
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-11-25 08:23:21 -0500

    Creating 'Footer' component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Button from './Components/Button/Button';
+import Footer from './Components/Footer/Footers';
 
 function App() {
   return (
     <div className="App">
       <Button />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footers.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+  render() {
+  return(
+    <div className="footer"> 
+      Eduardo Macetas
+      <a href="https://devchallenges.io/"> @DevChallenges.io </a> 
+    </div>
+    )
+  }
+}
+
+export default Footer;

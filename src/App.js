30d0577c310c6a2775a0fa31c109eecacaac6417
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-11-27 20:17:25 -0500

    Adding props and calling all components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Button from './Components/Button/Button';
-import Footer from './Components/Footer/Footers';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Page from './Components/Page/Page';
 
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Header />
+      <Page />  
       <Footer />
     </div>
   );
